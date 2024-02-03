66b1da89210849a003d815ad6ddcdca3f272353e
fmaldonadopetite <fj_maldonado@outlook.com>
2024-02-03 18:24:39 -0300

    Creación de build script

--- src/algebraLineal.c
+++ src/algebraLineal.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <stdint.h>
 
-typedef int8_t i8;
+typedef int8_t i8;      //char
+typedef uint8_t u8;
+typedef int16_t i16;    //short
+typedef uint16_t u16;
+typedef int32_t i32;    //int, long (size of pointer),
+typedef uint32_t u32;   //size_t
+typedef int64_t i64;    //long long
+typedef uint64_t u64;   //pointer
 
 
 int main(void){
